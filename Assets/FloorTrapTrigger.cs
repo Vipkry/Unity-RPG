44cb635aa4499bcffef6eb84dcdb26f49d225bd2
vipkry <lolpsxvipkry2@gmail.com>
2016-10-09 23:42:42 -0300

    added traps prototypes and light enviroments

---
+++ Assets/FloorTrapTrigger.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloorTrapTrigger : MonoBehaviour {
+
+	public LayerMask whatIsPlayer;
+	private SpriteRenderer spriteRenderer;
+	private bool triggered;
+
+	// Use this for initialization
+	void Start () {
+		spriteRenderer = GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Check if player stepped on the trap
+		if (!triggered && Physics2D.Raycast(new Vector2 (transform.position.x - spriteRenderer.bounds.extents.x, transform.position.y), Vector2.right, spriteRenderer.bounds.extents.x * 2)){
+			triggered = true;
+			// Activate trap
+				// Summon laser prefab and add velocity
+			// "Triggered"
+			Vector3 aux = transform.position;
+			aux.y -= spriteRenderer.bounds.extents.y;
+			transform.position = aux;
+		}
+
+	}
+}
